0b20cb74c5947c9b64a15156c973d0e3d9bdfce4
cocoto <marilun.lune@gmail.com>
2020-02-13 02:21:17 +0900

    2020-02-13 02:21

--- app/views/data/head.php
+++ app/views/data/head.php
@@ -5,7 +5,7 @@
 <meta property='og:title' content='Online Clock'>
 <meta property='og:description' content='The simplest clock, alarm and timer'>
 <meta property='og:image' content='/images/favicon/512-512.png'>
-<meta property='og:url' content='http://onlineclock.coccoto.com/'>
+<meta property='og:url' content='http://clock.coccoto.com/'>
 <meta name='twitter:card' content='summary'>
 <meta name='twitter:image' content='/images/favicon/512-512.png'>
 <meta name='twitter:creator' content='@coccoto_mono'>
