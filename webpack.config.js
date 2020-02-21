a1677ad9a8a215ba0d634465baee70050255f8db
cocoto <marilun.lune@gmail.com>
2020-02-21 22:21:05 +0900

    2020-02-21 22:21

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,8 @@ module.exports = () => {
             watchContentBase: true,
         },
         module: {
-            rules: [{
+            rules: [
+                {
                     test: /\.(js|jsx)$/,
                     exclude: /node_modules/,
                     loader: 'babel-loader',
