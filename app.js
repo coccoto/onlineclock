c15ce77f2f694bd922f28e83768c05bb6b510eec
coccoto <soprano.lune@gmail.com>
2020-06-29 00:17:09 +0900

    2020-06-29 00:17

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const path = require('path')
 
 const app = express()
 
+const hostname = 'localhost'
+const port = 3000
+
 // static setup
 app.use(express.static(path.resolve(__dirname, 'dist')))
 
@@ -10,6 +13,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
 })
 
-app.listen(3000, () => {
-    console.log('server running')
+app.listen(port, hostname, () => {
+    console.log('Server running at http://' + hostname + ':' + port　+　'/')
 })
